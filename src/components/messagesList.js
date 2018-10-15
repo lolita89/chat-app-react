72c2545b5bcc03703dc0a82b69a88a99e497539b
lolita89 <cika.lolita@hotmail.com>
2018-10-15 16:58:01 +0200

    created chat app components

---
+++ src/components/messagesList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MessagesList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                MessagesList
+            </div>
+        );
+    }
+}
+
+MessagesList.propTypes = {};
+
+export default MessagesList;
---
+++ src/components/title.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>title
+                
+            </div>
+        );
+    }
+}
+
+Title.propTypes = {};
+
+export default Title;
