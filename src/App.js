0cd10b760f8b072b4dfb817550b30aed4db015ea
lolita89 <cika.lolita@hotmail.com>
2018-10-17 16:39:22 +0200

    message appeared from dashboard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Title from './components/title';
 import MessagesList from './components/messagesList';
 import SendMessageForm from './components/sendMessageForm';
-import { Chatkit, ChatManager,TokenProvider } from '@pusher/chatkit';
+import Chatkit from '@pusher/chatkit';
 
 const DUMMY_DATA = [{
   senderId: "Luit",
@@ -19,6 +19,7 @@ const instanceLocator = "v1:us1:f2fe07b3-2a4b-49b7-8178-0b970efc6a72"
 const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/f2fe07b3-2a4b-49b7-8178-0b970efc6a72/token"
 const username = "Ciluit"
 const roomId = 18653904
+const userId = "Cilu"
 
 class App extends React.Component {
   constructor() {
@@ -28,58 +29,32 @@ class App extends React.Component {
     }
   }
 
-  // componentDidMount() {
-  //   const chatManager = new Chatkit.ChatManager({
-  //     instanceLocator: instanceLocator,
-  //     userId: username,
-  //     tokenProvider: new Chatkit.TokenProvider({
-  //       url: testToken
-  //     })
-  //   })
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: userId,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    })
 
-  //   chatManager.connect()
-  //   .then(currentUser => {
-  //     this.currentUser = currentUser
-  //     this.currentUser.subscribeToRoom({
-  //       roomId: roomId,
-  //       hooks: {
-  //         onNewMessage: message => {
-  //           this.setState({
-  //             messages: [...this.state.messages, message]
-  //           })
-  //         }
+    chatManager.connect()
+    .then(currentUser => {
+      this.currentUser = currentUser
+      this.currentUser.subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+          onNewMessage: message => {
+            this.setState({
+              messages: [...this.state.messages, message]
+            })
+          }
 
-  //       }
-  //     })
-  //   })
+        }
+      })
+    })
 
-  // }
-
-  // componentDidMount() {
-  //   const chatManager = new Chatkit.ChatManager({
-  //     instanceLocator: instanceLocator,
-  //     userId: username,
-  //     tokenProvider: new Chatkit.TokenProvider({
-  //       url: testToken
-  //     })
-  //  })  
-
-//    chatManager.connect()
-//    .then(currentUser => {
-//        this.currentUser = currentUser
-//        this.currentUser.subscribeToRoom({
-//        roomId: roomId,
-//        hooks: {
-//            onNewMessage: message => {
-
-//                this.setState({
-//                    messages: [...this.state.messages, message]
-//                })
-//            }
-//        }
-//    })
-//  })
-// }
+  }
 
   render() {
     return (
